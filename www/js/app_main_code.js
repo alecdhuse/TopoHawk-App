dd55e35aa66ada67aa6eb4fa42722c84023db919
Alec Dhuse <alec@dhuse.com>
2015-05-04 18:10:23 -0400

    Help comment formatting fix
    Help comment formatting fix

--- www/js/app_main_code.js
+++ www/js/app_main_code.js
@@ -1375,7 +1375,11 @@ function show_map_edit_buttons(is_new) {
     $("#target_overlay").css({'top':  target_top});
     $("#target_overlay").css({'left':  target_left});
     
-    show_help_comment("Position the target over the new route's location.");
+    if ($(window).width() < 420) {
+        show_help_comment("Position the target over the <br /> new route's location.");
+    } else {
+        show_help_comment("Position the target over the new route's location.");
+    }
     
     if (current_mode == MODE_AREA) {
         current_edit_mode = EDIT_MODE_ROUTE;
